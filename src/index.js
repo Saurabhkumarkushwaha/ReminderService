51435d6ed641b9f504beef3f5e586137a0c3cc4b
Saurabh Kushwaha <sk2005@gmail.com>
2025-05-25 09:06:42 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+//const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 
 const  setupAndStartServer = () => {
     const app = express();
@@ -13,12 +15,15 @@ const  setupAndStartServer = () => {
     app.listen(PORT , () => {
         console.log(`Server Started at port ${PORT}`);
 
-        sendBasicEmail(
-            'saurabh620548@gmail.com',
-            'sk62054847@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'saurabh620548@gmail.com',
+        //     'sk62054847@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minute');
+        });
     });
 }
 
