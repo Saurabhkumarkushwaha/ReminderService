636e8320e023733bccb13bf4ad2693af2b779d0f
Saurabh Kushwaha <sk2005@gmail.com>
2025-05-26 10:38:09 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -4,26 +4,27 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 //const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
 
-const cron = require('node-cron');
+const jobs = require('./utils/job');
 
 const  setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT , () => {
         console.log(`Server Started at port ${PORT}`);
-
+        jobs();
         // sendBasicEmail(
         //     'saurabh620548@gmail.com',
         //     'sk62054847@gmail.com',
         //     'This is a testing email',
         //     'Hey, how are you, I hope you like the support'
         // );
-        cron.schedule('*/2 * * * *', () => {
-            console.log('running a task every two minute');
-        });
+        
     });
 }
 
